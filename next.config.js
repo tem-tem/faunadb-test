6640f9e79019e10fe01610ac02f116602e76b447
TemTem <t3mir1an@gmail.com>
2020-02-08 22:08:18 -0600

    set env var in now.json

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ dotenvLoad("staging");
 
 module.exports = {
   env: {
-    FAUNADB_SECRET: process.env.FAUNADB_SECRET,
-    FAUNADB_SECRET_CURRENT: process.env.FAUNADB_SECRET_CURRENT
+    FAUNADB_SECRET: process.env.FAUNADB_SECRET
   }
 };
