4d8eed3a47dc54c68e9e601301c73909fe4bb893
TemTem <t3mir1an@gmail.com>
2020-02-10 21:36:31 -0600

    call next build with env var

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 // const dotenvLoad = require("dotenv-load");
 
-// module.exports = {
-//   env: {
-//     FAUNADB_SECRET: process.env.FAUNADB_SECRET,
-//     FAUNADB_SECRET_CURRENT: process.env.FAUNADB_SECRET_CURRENT
-//   }
-// };
+module.exports = {
+  env: {
+    FAUNADB_SECRET: process.env.FAUNADB_SECRET,
+    FAUNADB_SECRET_CURRENT: process.env.FAUNADB_SECRET_CURRENT
+  }
+};
