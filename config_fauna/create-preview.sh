a18d1901424e7b4884b4b84130cf58e919d4358d
TemTem <t3mir1an@gmail.com>
2020-02-11 02:38:12 -0600

    add create-db

--- config_fauna/create-preview.sh
+++ config_fauna/create-preview.sh
@@ -7,7 +7,7 @@ read_var() {
   then
     # file not found
     # returning env var with that name
-    echo "$2 not found"
+    echo "$2 not found" >&2
     exit 1
   else
     VAR=$(grep $1 $2 | xargs)
@@ -18,21 +18,23 @@ read_var() {
 
 getDBKey()
 {
-  if NODE_ENV=='production';
+  if [$1 == "prod"];
   then
     echo $FAUNADB_SECRET
     exit
   else
-    echo read_var FAUNADB_SECRET ./.env.development
+    echo $( read_var FAUNADB_SECRET ./.env.development )
   fi
 }
 
 authorizeFauna()
 {
-  echo "Authenticating with key: $key..."
-  echo $key | fauna cloud-login
-  # check auth
-  if ! fauna list-databases; then exit 1; fi
+  if ! fauna list-databases;
+  then
+    echo "Authenticating with key: $key..."
+    echo $key | fauna cloud-login
+    exit
+  fi
 }
 
 createKey()
@@ -63,7 +65,7 @@ seed()
     exit
   fi
 
-  seeds=$( node $seedFile $NOW_GITHUB_COMMIT_SHA )
+  seeds=$( node $seedFile $2 )
   if [ -z "$seeds" ]
   then
     echo "Error: Seed file not returning seeds. It should return graphql readable string query"
@@ -75,18 +77,24 @@ seed()
   echo -e '\n\nSeeding Complete.'
 }
 
+writeENV()
+{
+  envVars="FAUNADB_SECRET=$1"
+  echo -e $envVars > ./.env.development.local
+}
+
 #
 # MAIN SCRIPT
 #
-
-databaseName=$( echo "$1" | tr / _)
+env=$1
+databaseName=$( echo "$2" | tr / _)
 if [ -z "$databaseName" ];
 then
   echo "Missing database name. Can't start db setup."
   exit 1
 fi
 
-key=$( getDBKey )
+key=$( getDBKey $env )
 if [ -z "$key" ];
 then
   echo "FAUNADB_SECRET key not found. Can't start db setup."
@@ -97,6 +105,14 @@ authorizeFauna $key
 if ! fauna create-database $databaseName; then exit 1; fi
 previewDBKey=$( createKey $databaseName )
 uploadSchema $databaseName $previewDBKey
-seed $previewDBKey
+seed $previewDBKey $databaseName
 
-FAUNADB_SECRET_CURRENT=$previewDBKey yarn next build
+if [$env = 'prod'];
+then
+  # app is deployed
+  FAUNADB_SECRET_PREVIEW=$previewDBKey yarn next build
+  exit
+else
+  # script run locally
+  writeENV $previewDBKey
+fi
