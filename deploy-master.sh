978f080e3c7b4377c8af4ef5d28e68e4a8e7d692
TemTem <t3mir1an@gmail.com>
2020-02-12 16:08:16 -0600

    fix scripts

--- deploy-master.sh
+++ deploy-master.sh
@@ -1,4 +1,4 @@
-if ["$CIRCLE_BRANCH" == "master"]
+if [ "$CIRCLE_BRANCH" == "master" ]
 then
   curl https://api.zeit.co/v1/integrations/deploy/QmPsnMBbiHHY8KXbpiG5e61fnNaux5mT1L48HoH35MYavj/gLzUFmcI73
 fi
